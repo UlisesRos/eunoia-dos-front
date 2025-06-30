802ce4e1aa29924d4b0b3084eab54e803b1f8fd4
Ulises Ros <ulisesros70@gmail.com>
2025-06-30 12:36:31 -0300

    solucion de problemas en el calendario

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const backendUrl = 'http://localhost:5000';
-// const backendUrl = 'https://eunoiaback.onrender.com'; 
+// const backendUrl = 'http://localhost:5000';
+const backendUrl = 'https://eunoiaback.onrender.com'; 
 
 export default backendUrl;
