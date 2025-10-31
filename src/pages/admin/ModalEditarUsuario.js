58143737e88c84e49c00249d3d955ecf8f3cd627
Ulises Ros <ulisesros70@gmail.com>
2025-10-31 13:20:30 -0300

    dias semanales 3

--- src/pages/admin/ModalEditarUsuario.js
+++ src/pages/admin/ModalEditarUsuario.js
@@ -103,6 +103,7 @@ const ModalEditarUsuario = ({ isOpen, onClose, user, onUsuarioActualizado }) => 
                     >
                         <option value="1">1 día</option>
                         <option value="2">2 días</option>
+                        <option value="2">3 días</option>
                     </Select>
                 </FormControl>
             </ModalBody>
