e732d532107befb501fedbc6ebc94f0ba883c089
Ulises Ros <ulisesros70@gmail.com>
2025-11-30 17:50:05 -0300

    cambios de horarios

--- src/pages/CalendarioPage.js
+++ src/pages/CalendarioPage.js
@@ -90,22 +90,11 @@ import logo from '../img/logos/faviconE.png';
         useEffect(() => {
             if (!user) return;
 
-            /*getUserSelections()
+            getUserSelections()
                 .then((data) => {
                     const { selections, changesThisMonth, originalSelections = [] } = data;
                     setUserSelectionsState(selections || []);
-                    setCambiosRestantes(2 - (changesThisMonth || 0));*/
-
-            getUserSelections().then((data) => {
-                const { selections, changesThisMonth, originalSelections = [] } = data;
-                setUserSelectionsState(selections || []);
-                
-                // Calcular límite basado en si tiene viernes
-                const tieneViernes = (selections || []).some(s => s.day === 'Viernes') || 
-                                    (originalSelections || []).some(s => s.day === 'Viernes');
-                const limiteMaximo = tieneViernes ? 3 : 2;
-                
-                setCambiosRestantes(limiteMaximo - (changesThisMonth || 0));
+                    setCambiosRestantes(2 - (changesThisMonth || 0));
 
                     // ADMIN: nunca ve modal ni banner
                     if (user.rol === 'admin') {
@@ -386,15 +375,8 @@ import logo from '../img/logos/faviconE.png';
                     display={user.rol === 'admin' ? 'none' : 'flex'}
                     >
                     {}
-                    {/*<Text fontWeight="bold">
-                        Cambios Mensuales Restantes: {cambiosRestantes} / 2
-                    </Text>*/}
-
                     <Text fontWeight="bold">
-                        Cambios Mensuales Restantes: {cambiosRestantes} / {
-                            (userSelections || []).some(s => s.day === 'Viernes') ? 3 : 2
-                        }
-                        {(userSelections || []).some(s => s.day === 'Viernes') && ' 🎉'}
+                        Cambios Mensuales Restantes: {cambiosRestantes} / 2
                     </Text>
 
                     <Text
